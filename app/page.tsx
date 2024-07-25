65db4392aa4fa7f3de7dada4c937c5a373113a93
Nikita Korneev <nickkorneevspace@gmail.com>
2024-07-25 20:09:23 +0300

    basic route architecture

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-	return (
-		<div className="container">
-			<h1 className="h1-bold">Next JS 14 with Tailwind</h1>
-			<h2 className="h2-bold">Next JS 14 with Tailwind</h2>
-			<h3 className="h3-bold">Next JS 14 with Tailwind</h3>
-		</div>
-	);
-}
