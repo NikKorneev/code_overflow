518aaee805f9925ff8349c0fb4f76d8383aac830
Nikita Korneev <nickkorneevspace@gmail.com>
2024-08-05 18:44:38 +0300

    questionPage waits for server action

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Editor } from "@tinymce/tinymce-react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { ControllerRenderProps, useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import {
@@ -17,6 +17,9 @@ import { Input } from "@/components/ui/input";
 import { questionsSchema } from "@/lib/validations";
 import { useEffect, useRef, useState } from "react";
 import { useTheme } from "@/context/ThemeProvider";
+import { Badge } from "../ui/badge";
+import Image from "next/image";
+import { type } from "os";
 
 const QuestionForm = () => {
 	const editorRef = useRef<null>(null);
@@ -26,13 +29,70 @@ const QuestionForm = () => {
 		resolver: zodResolver(questionsSchema),
 		defaultValues: {
 			title: "",
+			explanation: "",
+			tags: [],
 		},
 	});
 
+	const { isSubmitting } = form.formState;
+
 	function onSubmit(values: z.infer<typeof questionsSchema>) {
 		console.log(values);
+		try {
+			//make an async to API -> create a question
+			//contain all form data
+			//navigate to home page
+		} catch (error) {}
 	}
-	console.log(themeContext.mode);
+
+	const handleInputKeyDown = (
+		e: React.KeyboardEvent<HTMLInputElement>,
+		field: ControllerRenderProps<
+			{
+				title: string;
+				explanation: string;
+				tags: string[];
+			},
+			"tags"
+		>
+	) => {
+		if (e.key === "Enter" && field.name === "tags") {
+			e.preventDefault();
+
+			const tagInput = e.target as HTMLInputElement;
+			const tagValue = tagInput.value.trim();
+
+			if (tagValue !== "") {
+				if (tagValue.length > 15) {
+					return form.setError("tags", {
+						type: "required",
+						message: "Tag must be less than 15 characters",
+					});
+				}
+
+				if (!form.getValues().tags?.includes(tagValue as never)) {
+					form.setValue("tags", [
+						...form.getValues()?.tags,
+						tagValue,
+					]);
+					tagInput.value = "";
+					form.clearErrors("tags");
+				} else {
+					return form.setError("tags", {
+						type: "required",
+						message: "Tag already contains in the list",
+					});
+				}
+			}
+		}
+	};
+
+	const handleDelete = (str: string) => {
+		form.setValue(
+			"tags",
+			form.getValues().tags?.filter((tag) => tag !== str)
+		);
+	};
 
 	return (
 		<Form {...form}>
@@ -195,11 +255,26 @@ const QuestionForm = () => {
 							</FormLabel>
 							<FormControl className="mt-3.5">
 								<Input
-									{...field}
+									onKeyDown={(e) =>
+										handleInputKeyDown(e, field)
+									}
 									placeholder="Add tags..."
 									className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
 								/>
 							</FormControl>
+							{form.getValues().tags?.length > 0 && (
+								<div className="flex-start mt-2.5 flex gap-2.5">
+									{form.getValues().tags?.map((tag) => (
+										<Badge
+											key={tag}
+											className="subtle-medium background-light800_dark300 text-light400_light500 cursor-pointer rounded-md border-none px-4 py-2 capitalize"
+											onClick={() => handleDelete(tag)}
+										>
+											<p>{tag}</p>
+										</Badge>
+									))}
+								</div>
+							)}
 							<FormDescription className="body-regular mt-2.5 text-light-500">
 								Add up to 3 tags to describe what your question
 								is about. You need to press Enter after each
@@ -209,7 +284,21 @@ const QuestionForm = () => {
 						</FormItem>
 					)}
 				/>
-				<Button type="submit">Submit</Button>
+				<Button
+					type="submit"
+					className="primary-gradient w-fit !text-light-900 "
+					disabled={isSubmitting}
+				>
+					{isSubmitting ? (
+						<>{type === "edit" ? "Editing..." : "Posting..."}</>
+					) : (
+						<>
+							{type === "edit"
+								? "Edit Question"
+								: "Ask a Question"}
+						</>
+					)}
+				</Button>
 			</form>
 		</Form>
 	);
--- lib/validations.ts
+++ lib/validations.ts
@@ -6,5 +6,12 @@ export const questionsSchema = z.object({
 		.min(5, { message: "Title must be at least 5 characters" })
 		.max(130, { message: "Title must be less than 130 characters" }),
 	explanation: z.string().min(20),
-	tags: z.array(z.string().min(1).max(15)).min(1).max(3),
+	tags: z
+		.array(z.string().min(1).max(15))
+		.min(1, {
+			message: "Add one more tag",
+		})
+		.max(3, {
+			message: "You can add only 3 tags",
+		}),
 });
