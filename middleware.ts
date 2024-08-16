95c6fc551c246642e44451ddf9d815c7d70a78f1
Nikita Korneev <nickkorneevspace@gmail.com>
2024-08-16 20:31:52 +0300

    profile edit ready

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/ask-question(.*)", "/profile"]);
+const isProtectedRoute = createRouteMatcher([
+	"/ask-question(.*)",
+	"/profile",
+	"/profile/edit",
+	"/question/edit(.*)",
+]);
 
 export default clerkMiddleware((auth, req) => {
 	if (isProtectedRoute(req)) auth().protect();
