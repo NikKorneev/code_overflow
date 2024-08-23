51ca0f0792d8f1ca3021da426ab9c1b47144f456
Nikita Korneev <nickkorneevspace@gmail.com>
2024-08-23 19:47:42 +0300

    ai generating feature is temporarily remove

--- components/forms/AnswerForm.tsx
+++ components/forms/AnswerForm.tsx
@@ -14,7 +14,6 @@ import { Editor } from "@tinymce/tinymce-react";
 import { useTheme } from "@/context/ThemeProvider";
 import { useRef, useState } from "react";
 import { Button } from "../ui/button";
-import Image from "next/image";
 import { createAnswer } from "@/lib/actions/answer.action";
 import { usePathname, useRouter } from "next/navigation";
 
@@ -58,70 +57,12 @@ const AnswerForm = ({ question, questionId, authorId }: Props) => {
 		}
 	};
 
-	const generateAIAnswer = async () => {
-		if (!authorId) return;
-
-		setIsSubmittingAi(true);
-
-		try {
-			const response = await fetch(
-				`${process.env.NEXT_PUBLIC_SERVER_URL}/api/chatgpt`,
-				{
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({
-						question,
-					}),
-				}
-			);
-
-			const aiAnswer = await response.json();
-
-			const formattedAnswer = aiAnswer?.reply?.replace(/\n/g, "<br />");
-
-			if (editorRef.current) {
-				const editor = editorRef.current as any;
-
-				editor.setContent(formattedAnswer);
-			}
-
-			//toast
-		} catch (error) {
-			console.log(error);
-		} finally {
-			setIsSubmittingAi(false);
-		}
-	};
-
 	return (
 		<div>
 			<div className="flex flex-col justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
 				<h4 className="paragraph-semibold text-dark400_light800">
 					Write your answer here
 				</h4>
-
-				<Button
-					className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500"
-					onClick={generateAIAnswer}
-					disabled={isSubmittingAi || form.formState.isSubmitting}
-				>
-					{isSubmittingAi ? (
-						<>Generating...</>
-					) : (
-						<>
-							<Image
-								src="/assets/icons/stars.svg"
-								alt="stars icon"
-								width={12}
-								height={12}
-								className="object-contain"
-							/>
-							Generate an AI Answer
-						</>
-					)}
-				</Button>
 			</div>
 
 			<Form {...form}>
